a08019922b57dc0146c755f5757fd4667116f05e
JonahLargen <jhlargen2@gmail.com>
2024-11-17 19:46:48 -0500

    Update Colors

--- shot.py
+++ shot.py
@@ -7,7 +7,7 @@ class Shot(CircleShape):
         super().__init__(x, y, radius)
         
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, SHOT_RADIUS)
+        pygame.draw.circle(screen, "#be2fb7", self.position, self.radius, SHOT_RADIUS)
         
     def update(self, dt):
         self.position += self.velocity * dt
